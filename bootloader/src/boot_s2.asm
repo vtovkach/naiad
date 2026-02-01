11caa611f3c6b36fcbf3ee8a4c9356861428f515
alexanderthegreat <vadim003600@gmail.com>
2026-01-31 20:47:52 -0600

    Added get_e820 function that returns memory status.

--- bootloader/src/boot_s2.asm
+++ bootloader/src/boot_s2.asm
@@ -2,13 +2,24 @@
 org 0x7E00
 bits 16 
 
+; ------------
+; CONSTANTS
+; ------------
+SMAP        equ 0x534D4150 
+E820_MAX    equ 64
+E820_ES     equ 0x0000
+
 start: 
-    ; Stage 2 Code goes here 
     
+    ; Display Success Transition Message 
     mov si, stage2_success_msg
     call printStatus
 
-    jmp done 
+    call get_e820
+
+    call display_e820
+
+    jmp halt
 
 ; Set SI to point to the message before the function call
 printStatus:
@@ -24,12 +35,73 @@ printStatus:
 .end_print:
     ret
 
+get_e820:
+    pushad 
+    push ds 
+    push es 
+
+    xor ax, ax 
+    mov ds, ax 
+
+    ; Point ES:DI at buffer 
+    mov ax, E820_ES
+    mov es, ax 
+    mov di, e820_buf
+
+    xor ebx, ebx 
+    xor bp, bp 
+    mov edx, SMAP
+
+.e820_next:
+    mov eax, 0xE820 
+    mov ecx, 24      ; 24 bytes per entry 
+    int 0x15
+    jc .done         ; CF=1 -> finished/error 
+
+    cmp eax, SMAP
+    jne .done        ; BIOS did't return SMAP -> abort
+
+    mov eax, [es:di+8]
+    or eax, [es:di+12]
+    jz .skip 
+
+    inc bp
+
+    add di, 24
+    cmp bp, E820_MAX
+    jae .done 
+
+.skip:
+    test ebx, ebx 
+    jne .e820_next 
+
+.done:
+    mov [e820_count], bp 
+
+    pop es
+    pop ds
+    popad 
+    ret
+
 ; Halt CPU 
-done: 
+halt: 
     cli 
     hlt 
 
-stage2_success_msg:
-    db "Second stage loaded successfully", 0x0D, 0x0A, 0
+; ---------------
+; Status Messages
+; --------------- 
+stage2_success_msg: db "Second stage loaded successfully", 0x0D, 0x0A, 0
+str_base: db "BASE=", 0
+str_len:  db " LEN=", 0
+str_type: db " TYPE=", 0
+
+; ------------------ 
+; E820 Data 
+; ------------------
+e820_count: dw 0
+e820_buf:
+    times(E820_MAX*24) db 0
+
 
 times (32*512) - ($-$$) db 0 
